e774af1c3a0c8ac5d8f6ca6ef9ae58eb9609c553
simon.frijns <simon.frijns@outlook.com>
2021-11-07 19:14:55 +0100

    formatting

--- day-13/code.py
+++ day-13/code.py
@@ -19,10 +19,8 @@ def part2(input: list) -> int:
     ans = 0
     busses = input[1].split(",")
     bus_intervals = [(int(bus), interval) for interval, bus in enumerate(busses) if bus != 'x']
-
     timestamp = 0
     step = bus_intervals[0][0]
-
     for bus, interval in bus_intervals[1:]:
         no_match_found = True
         while no_match_found:
@@ -31,7 +29,6 @@ def part2(input: list) -> int:
                 break # need to break to prevent adding another step
             timestamp += step
         step = math.lcm(step, bus) # step should be whatever is least common multiple between all numbers until now and current bus
-    
     ans = timestamp
     return ans
 
